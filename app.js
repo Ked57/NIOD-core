c071d38ce3172a5ea7ff9790b9c720051de710a1
Ked <kedketh@gmail.com>
2018-10-12 11:40:00 +0200

    Basic app implementation

---
+++ app.js
@@ -0,0 +1,33 @@
+var net = require("net");
+const express = require("express");
+const app = express();
+
+const _PORT = 15487;
+const _HOST = "127.0.0.1";
+
+var client = new net.Socket();
+
+client.connect(
+  _PORT,
+  _HOST,
+  () => {
+    console.log("Connected");
+    client.write("Hello, server!");
+  }
+);
+
+client.on("data", data => {
+  console.log("Received: " + data);
+});
+
+client.on("close", () => {
+  console.log("Connection closed");
+});
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
