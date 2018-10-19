3a984fe6db34b468c9eb58954d3cfd83466a11f4
Clément Fassot <kedketh@gmail.com>
2018-10-19 21:49:07 +0200

    Add a vuejs frontend

---
+++ front/src/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+<h1>Welcome to {{server}}, a DCS server</h1>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      server: 'NIOD',
+    };
+  },
+};
+</script> 
---
+++ front/src/components/Tests.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>Tests page</h1>
+</template>
+
+<script>
+export default {
+  name: 'tests',
+  data() {
+    return {};
+  },
+};
+</script> 
